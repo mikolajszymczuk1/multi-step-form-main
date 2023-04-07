25c8bdfd7ca12c186dbbc5af903446f5605ee9ed
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-04-07 03:16:54 +0200

    Created separated step card component to wrap all step cards

---
+++ src/components/cards/StepCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="stepCard">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss">
+.stepCard {
+  margin: 0 auto;
+  padding: 32px 24px;
+
+  width: 91%;
+
+  background-color: $White;
+  border-radius: 10px;
+  transform: translateY(-73px);
+  box-shadow: 0 25px 40px -20px rgba(black, 0.1);
+}
+</style>
